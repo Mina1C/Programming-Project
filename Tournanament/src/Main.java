1b2fd15bda7b47bd07704b2f9b53e27fdfc4904b
Mina <349763789@gapps.yrdsb.ca>
2025-01-09 13:35:42 -0500

    Team names are taken from user and stored in an Array called names. The program checks for uniqueness of the names, if not unique a message is displayed and the user can enter another name. At the end the names of the teams are displayed.

--- Tournanament/src/Main.java
+++ Tournanament/src/Main.java
@@ -1,7 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
 
+        System.out.println("Enter the name of the 8 Teams:");
+        String [] names = new String [8]; //I used an Array because I know the number of elements I want (8)
+
+        for (int i = 0; i < 8; i++){
+            int notUnique = 0; //acts as a counter to check for uniqueness
+            String str = sc.nextLine();
+
+            for (int j = 0; j < i; j++){
+                if (str.equals(names[j])){
+                    notUnique++;
+                }
+            }
+            if (notUnique != 0) {
+                i--;  //if the name entered already exists, then the user can re-enter
+                System.out.println("Not a unique name, enter another name:");
+            }
+            else
+                names[i] = str;
+        }
+        System.out.println("The 8 teams are: "+Arrays.toString(names)); //confirms and displays the team names
+
+
+
+
+        sc.close();
     }
 }
